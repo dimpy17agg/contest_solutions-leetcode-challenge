3b28d82df8a261544b3a546bd205a10a10a6f564
Dimpy Aggarwal <47886241+dimpy17agg@users.noreply.github.com>
2020-06-20 22:34:26 +0530

    day20 //Permutation Sequence//

---
+++ June LeetCode/week3/PermutationSequence.java
@@ -0,0 +1,64 @@
+/*The set [1,2,3,...,n] contains a total of n! unique permutations.
+
+By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+
+"123"
+"132"
+"213"
+"231"
+"312"
+"321"
+Given n and k, return the kth permutation sequence.
+
+Example 1:
+Input: n = 3, k = 3
+Output: "213"
+*/
+class Solution {
+    public String getPermutation(int n, int k) {
+        
+        
+        ArrayList<Integer> numberList = new ArrayList<Integer>();
+		for (int i = 1; i <= n; i++) {
+			numberList.add(i);
+		}
+ 
+		// change k to be index
+		k--;
+ 
+		// set factorial of n
+		int mod = 1;
+		for (int i = 1; i <= n; i++) {
+			mod = mod * i;
+		}
+ 
+		String result = "";
+ 
+		// find sequence
+		for (int i = 0; i < n; i++) {
+			mod = mod / (n - i);
+			// find the right number(curIndex) of
+			int curIndex = k / mod;
+			// update k
+			k = k % mod;
+
+			// get number according to curIndex
+			result += numberList.get(curIndex);
+			// remove from list
+			numberList.remove(curIndex);
+		}
+ 
+		return result.toString();
+    
+    }
+}
+
+
+
+
+
+
+
+
+
+
