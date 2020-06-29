f6b5b7ebe747efe438bdc14b71b7aa6cbb122e0f
Dimpy Aggarwal <47886241+dimpy17agg@users.noreply.github.com>
2020-06-29 19:06:28 +0530

    day29 //Unique Paths//

---
+++ June LeetCode/week5/UniquePaths.java
@@ -0,0 +1,27 @@
+/*
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+How many possible unique paths are there?
+
+Example 1:
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+*/
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if(m==0||n==0) return 0;
+        int[][] f=new int[m][n];
+        for(int i=0;i<m;i++) f[i][0]=1;
+        for(int j=0;j<n;j++) f[0][j]=1;
+        for(int i=1;i<m;i++)
+            for(int j=1;j<n;j++)
+                f[i][j]=f[i-1][j]+f[i][j-1];
+        return f[m-1][n-1];   
+    }
+}
